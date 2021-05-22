84062b61421bdf6118a8c070d8e37d0f71eb9a81
Susana Santos Moreno <susiylola@gmail.com>
2021-05-22 10:52:00 +0200

    update to AWS with mysql

--- EcommerceSocksAPI/Startup.cs
+++ EcommerceSocksAPI/Startup.cs
@@ -25,10 +25,13 @@ namespace EcommerceSocksAPI {
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices (IServiceCollection services) {
-            String cadena = this.Configuration.GetConnectionString("azure");
+            String cadena = this.Configuration.GetConnectionString("AWS");
             services.AddTransient<Ecommerce_socksRepository>();
             services.AddTransient<HelperToken>();
-            services.AddDbContext<Ecommerce_socksContext>(options => options.UseSqlServer(cadena));
+            services.AddDbContextPool<Ecommerce_socksContext>(
+                options => options.UseMySql(cadena, ServerVersion.AutoDetect(cadena)));
+
+            services.AddCors(options => options.AddPolicy("AllowOrigin", c => c.AllowAnyOrigin()));
 
             services.AddSwaggerGen(options => {
                 options.SwaggerDoc(name: "v1", new OpenApiInfo {
@@ -37,6 +40,7 @@ namespace EcommerceSocksAPI {
                     Description = "Api Ecommerce Socks 2021"
                 });
             });
+
             HelperToken helpers = new HelperToken(this.Configuration);
             services.AddAuthentication(helpers.GetAuthOptions()).AddJwtBearer(helpers.GetJwtBearerOptions());
             services.AddControllers();
@@ -46,6 +50,8 @@ namespace EcommerceSocksAPI {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(options => options.AllowAnyOrigin());
+            app.UseHttpsRedirection();
             app.UseSwagger();
             app.UseSwaggerUI(options => {
                 options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Api v1");
